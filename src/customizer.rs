14499a0e4fb3d3494efeb49a891f0d53c0e65049
Rob Tandy <rob.tandy@datadoghq.com>
2025-07-16 09:09:13 -0400

    format change

--- src/customizer.rs
+++ src/customizer.rs
@@ -4,10 +4,10 @@ use datafusion_proto::physical_plan::PhysicalExtensionCodec;
 #[async_trait::async_trait]
 pub trait Customizer: PhysicalExtensionCodec + Send + Sync {
     /// Customize the context before planning a query.
-    /// This may include registering new file formats or introducing additional 
-    /// `PhysicalPlan` operators. 
+    /// This may include registering new file formats or introducing additional
+    /// `PhysicalPlan` operators.
     ///
-    /// To support serialization of customized plans for distributed execution, 
+    /// To support serialization of customized plans for distributed execution,
     /// a `Codec` may also be required.
     async fn customize(&self, ctx: &mut SessionContext) -> Result<(), Box<dyn std::error::Error>>;
 }
