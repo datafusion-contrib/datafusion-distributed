7253ed58c971c096dc502f6fa274dcdeb1d1b18b
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2025-07-31 08:55:08 +0200

    Bring tests from https://github.com/gabotechs/datafusion-distributed-experiment

---
+++ tests/common/localhost.rs
@@ -0,0 +1,122 @@
+use arrow_flight::flight_service_server::FlightServiceServer;
+use async_trait::async_trait;
+use datafusion::common::runtime::JoinSet;
+use datafusion::common::DataFusionError;
+use datafusion::execution::SessionStateBuilder;
+use datafusion::prelude::SessionContext;
+use datafusion_distributed_experiment::{
+    ArrowFlightChannel, ArrowFlightEndpoint, BoxCloneSyncChannel, ChannelManager, ChannelResolver,
+    SessionBuilder,
+};
+use std::error::Error;
+use std::sync::atomic::AtomicUsize;
+use std::sync::Arc;
+use std::time::Duration;
+use tonic::transport::{Channel, Server};
+use url::Url;
+
+#[derive(Debug, Clone)]
+pub struct NoopSessionBuilder;
+impl SessionBuilder for NoopSessionBuilder {
+    fn on_new_session(&self, builder: SessionStateBuilder) -> SessionStateBuilder {
+        builder
+    }
+}
+
+pub async fn start_localhost_context<N, I, B>(
+    ports: I,
+    session_builder: B,
+) -> (SessionContext, JoinSet<()>)
+where
+    N::Error: std::fmt::Debug,
+    N: TryInto<u16>,
+    I: IntoIterator<Item = N>,
+    B: SessionBuilder + Send + Sync + 'static,
+    B: Clone,
+{
+    let ports: Vec<u16> = ports.into_iter().map(|x| x.try_into().unwrap()).collect();
+    let channel_resolver = LocalHostChannelResolver::new(ports.clone());
+    let mut join_set = JoinSet::new();
+    for port in ports {
+        let channel_resolver = channel_resolver.clone();
+        let session_builder = session_builder.clone();
+        join_set.spawn(async move {
+            spawn_flight_service(channel_resolver, session_builder, port)
+                .await
+                .unwrap();
+        });
+    }
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    let ctx = SessionContext::new();
+
+    ctx.state_ref()
+        .write()
+        .config_mut()
+        .set_extension(Arc::new(ChannelManager::new(channel_resolver)));
+
+    (ctx, join_set)
+}
+
+#[derive(Clone)]
+pub struct LocalHostChannelResolver {
+    ports: Vec<u16>,
+    i: Arc<AtomicUsize>,
+}
+
+impl LocalHostChannelResolver {
+    pub fn new<N: TryInto<u16>, I: IntoIterator<Item = N>>(ports: I) -> Self
+    where
+        N::Error: std::fmt::Debug,
+    {
+        Self {
+            i: Arc::new(AtomicUsize::new(0)),
+            ports: ports.into_iter().map(|v| v.try_into().unwrap()).collect(),
+        }
+    }
+}
+
+#[async_trait]
+impl ChannelResolver for LocalHostChannelResolver {
+    async fn get_n_channels(&self, n: usize) -> Result<Vec<ArrowFlightChannel>, DataFusionError> {
+        let mut result = vec![];
+        for _ in 0..n {
+            let i = self.i.fetch_add(1, std::sync::atomic::Ordering::SeqCst) % self.ports.len();
+            let port = self.ports[i];
+            let url = format!("http://localhost:{port}");
+            let endpoint = Channel::from_shared(url.clone()).map_err(external_err)?;
+            let channel = endpoint.connect().await.map_err(external_err)?;
+            result.push(ArrowFlightChannel {
+                url: Url::parse(&url).map_err(external_err)?,
+                channel: BoxCloneSyncChannel::new(channel),
+            })
+        }
+        Ok(result)
+    }
+
+    async fn get_channel_for_url(&self, url: &Url) -> Result<ArrowFlightChannel, DataFusionError> {
+        let endpoint = Channel::from_shared(url.to_string()).map_err(external_err)?;
+        let channel = endpoint.connect().await.map_err(external_err)?;
+        Ok(ArrowFlightChannel {
+            url: url.clone(),
+            channel: BoxCloneSyncChannel::new(channel),
+        })
+    }
+}
+
+pub async fn spawn_flight_service(
+    channel_resolver: impl ChannelResolver + Send + Sync + 'static,
+    session_builder: impl SessionBuilder + Send + Sync + 'static,
+    port: u16,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut endpoint = ArrowFlightEndpoint::new(channel_resolver);
+    endpoint.with_session_builder(session_builder);
+    Ok(Server::builder()
+        .add_service(FlightServiceServer::new(endpoint))
+        .serve(format!("127.0.0.1:{port}").parse()?)
+        .await?)
+}
+
+fn external_err(err: impl Error + Send + Sync + 'static) -> DataFusionError {
+    DataFusionError::External(Box::new(err))
+}
---
+++ tests/distributed_aggregation.rs
@@ -0,0 +1,94 @@
+mod common;
+
+#[cfg(test)]
+mod tests {
+    use crate::assert_snapshot;
+    use crate::common::localhost::{start_localhost_context, NoopSessionBuilder};
+    use crate::common::parquet::register_parquet_tables;
+    use crate::common::plan::distribute_aggregate;
+    use datafusion::arrow::util::pretty::pretty_format_batches;
+    use datafusion::physical_plan::{displayable, execute_stream};
+    use futures::TryStreamExt;
+    use std::error::Error;
+
+    #[tokio::test]
+    async fn distributed_aggregation() -> Result<(), Box<dyn Error>> {
+        let (ctx, _guard) =
+            start_localhost_context([50050, 50051, 50052], NoopSessionBuilder).await;
+        register_parquet_tables(&ctx).await?;
+
+        let df = ctx
+            .sql(r#"SELECT count(*), "RainToday" FROM weather GROUP BY "RainToday" ORDER BY count(*)"#)
+            .await?;
+        let physical = df.create_physical_plan().await?;
+        let physical_str = displayable(physical.as_ref()).indent(true).to_string();
+
+        let physical_distributed = distribute_aggregate(physical.clone())?;
+        let physical_distributed_str = displayable(physical_distributed.as_ref())
+            .indent(true)
+            .to_string();
+
+        assert_snapshot!(physical_str,
+            @r"
+        ProjectionExec: expr=[count(*)@0 as count(*), RainToday@1 as RainToday]
+          SortPreservingMergeExec: [count(Int64(1))@2 ASC NULLS LAST]
+            SortExec: expr=[count(Int64(1))@2 ASC NULLS LAST], preserve_partitioning=[true]
+              ProjectionExec: expr=[count(Int64(1))@1 as count(*), RainToday@0 as RainToday, count(Int64(1))@1 as count(Int64(1))]
+                AggregateExec: mode=FinalPartitioned, gby=[RainToday@0 as RainToday], aggr=[count(Int64(1))]
+                  CoalesceBatchesExec: target_batch_size=8192
+                    RepartitionExec: partitioning=Hash([RainToday@0], CPUs), input_partitions=CPUs
+                      RepartitionExec: partitioning=RoundRobinBatch(CPUs), input_partitions=1
+                        AggregateExec: mode=Partial, gby=[RainToday@0 as RainToday], aggr=[count(Int64(1))]
+                          DataSourceExec: file_groups={1 group: [[/testdata/weather.parquet]]}, projection=[RainToday], file_type=parquet
+        ",
+        );
+
+        assert_snapshot!(physical_distributed_str,
+            @r"
+        ProjectionExec: expr=[count(*)@0 as count(*), RainToday@1 as RainToday]
+          SortPreservingMergeExec: [count(Int64(1))@2 ASC NULLS LAST]
+            SortExec: expr=[count(Int64(1))@2 ASC NULLS LAST], preserve_partitioning=[true]
+              ProjectionExec: expr=[count(Int64(1))@1 as count(*), RainToday@0 as RainToday, count(Int64(1))@1 as count(Int64(1))]
+                AggregateExec: mode=FinalPartitioned, gby=[RainToday@0 as RainToday], aggr=[count(Int64(1))]
+                  ArrowFlightReadExec: input_actors=8
+                    CoalesceBatchesExec: target_batch_size=8192
+                      RepartitionExec: partitioning=Hash([RainToday@0], CPUs), input_partitions=CPUs
+                        RepartitionExec: partitioning=RoundRobinBatch(CPUs), input_partitions=1
+                          AggregateExec: mode=Partial, gby=[RainToday@0 as RainToday], aggr=[count(Int64(1))]
+                            ArrowFlightReadExec: input_actors=1 hash=[RainToday@0]
+                              DataSourceExec: file_groups={1 group: [[/testdata/weather.parquet]]}, projection=[RainToday], file_type=parquet
+        ",
+        );
+
+        let batches = pretty_format_batches(
+            &execute_stream(physical, ctx.task_ctx())?
+                .try_collect::<Vec<_>>()
+                .await?,
+        )?;
+
+        assert_snapshot!(batches, @r"
+        +----------+-----------+
+        | count(*) | RainToday |
+        +----------+-----------+
+        | 66       | Yes       |
+        | 300      | No        |
+        +----------+-----------+
+        ");
+
+        let batches_distributed = pretty_format_batches(
+            &execute_stream(physical_distributed, ctx.task_ctx())?
+                .try_collect::<Vec<_>>()
+                .await?,
+        )?;
+        assert_snapshot!(batches_distributed, @r"
+        +----------+-----------+
+        | count(*) | RainToday |
+        +----------+-----------+
+        | 66       | Yes       |
+        | 300      | No        |
+        +----------+-----------+
+        ");
+
+        Ok(())
+    }
+}
