14204f839fef5b2ab593c39af290a04aeb37e054
Jayant Shrivastava <jayant.shrivastava@datadoghq.com>
2025-10-08 15:53:58 -0400

    working example

--- src/execution_plans/network_boundary_builder.rs
+++ src/execution_plans/network_boundary_builder.rs
@@ -1,7 +1,17 @@
-use std::{any::Any, sync::Arc};
+use std::fmt::Formatter;
+use std::sync::Arc;
+use datafusion::execution::TaskContext;
+use datafusion::physical_plan::metrics::MetricsSet;
+use datafusion::physical_plan::DisplayFormatType;
 use datafusion::physical_plan::ExecutionPlan;
+use datafusion::physical_plan::PlanProperties;
 use crate::distributed_physical_optimizer_rule::{NetworkBoundary, NetworkBoundaryExt};
-
+use delegate::delegate;
+use datafusion::error::Result;
+use datafusion::physical_plan::SendableRecordBatchStream;
+use std::any::Any;
+use datafusion::physical_plan::ExecutionPlanProperties;
+use datafusion::physical_plan::DisplayAs;
 
 // Use HRTB to work with any lifetime
 type DowncastFn = for<'a> fn(&'a dyn ExecutionPlan) -> Option<&'a dyn NetworkBoundaryExt>;
@@ -39,30 +49,97 @@ impl NetworkBoundaryRegistry {
                 }
             }
         }
-        // None
-        plan.as_network_boundary()
+        None
     }
 }
 
+#[derive(Debug)]
 struct MyWrapper{
-    plan: Arc<dyn ExecutionPlan>,
+    inner: Arc<dyn ExecutionPlan>,
+}
+
+impl DisplayAs for MyWrapper {
+    fn fmt_as(&self, fmt_type: DisplayFormatType, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MyWrapper")
+    }
+}
+
+impl ExecutionPlan for MyWrapper {
+    fn name(&self) -> &str {
+        "MetricsWrapperExec"
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn properties(&self) -> &PlanProperties {
+        unimplemented!("MetricsWrapperExec does not implement properties")
+    }
+
+    fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
+        self.inner.children()
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        children: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> Result<Arc<dyn ExecutionPlan>> {
+        unimplemented!("MetricsWrapperExec does not implement with_new_children")
+    }
+
+    fn execute(
+        &self,
+        _partition: usize,
+        _contex: Arc<TaskContext>,
+    ) -> Result<SendableRecordBatchStream> {
+        unimplemented!("MetricsWrapperExec does not implement execute")
+    }
+
+    // metrics returns the wrapped metrics.
+    fn metrics(&self) -> Option<MetricsSet> {
+        self.inner.metrics()
+    }
 }
 
 impl NetworkBoundaryExt for MyWrapper {
     fn as_network_boundary(&self) -> Option<&dyn NetworkBoundary> {
-        None
+        println!("calling as_network_boundary on inner");
+       self.inner.as_network_boundary() 
     }
     fn is_network_boundary(&self) -> bool {
-        self.as_network_boundary().is_some()
+        println!("calling is_network_boundary on inner");
+        self.inner.as_network_boundary().is_some()
     }
 }
 
 // Usage example:
 mod tests {
+    use std::sync::Arc;
+
+    use datafusion::physical_plan::empty::EmptyExec;
+    use arrow::datatypes::Schema;
+    use arrow::datatypes::Field;
+    use arrow::datatypes::DataType;
+
     use crate::{execution_plans::network_boundary_builder::{MyWrapper, NetworkBoundaryRegistry}};
 
-    fn test() {
+    #[test]
+    fn test_asdfasdf() {
         let registry = NetworkBoundaryRegistry::new()
             .register::<MyWrapper>();
+
+
+            let plan = EmptyExec::new(
+                Arc::new(Schema::new(
+                    vec![Field::new("a", DataType::Int64, false)],
+                )),
+            );
+
+            let plan2 = MyWrapper {
+                inner: Arc::new(plan)
+            };
+
+        let b = registry.try_downcast(&plan2);
     }
 }
