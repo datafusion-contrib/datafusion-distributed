80b2d967f59910f077361538786e95b948be2475
Jayant Shrivastava <jayant.shrivastava@datadoghq.com>
2025-10-14 17:31:15 -0400

    wip

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -4,4 +4,4 @@ mod task_metrics_collector;
 mod task_metrics_rewriter;
 pub(crate) use metrics_collecting_stream::MetricsCollectingStream;
 pub(crate) use task_metrics_collector::{MetricsCollectorResult, TaskMetricsCollector};
-pub(crate) use task_metrics_rewriter::TaskMetricsRewriter;
+pub(crate) use task_metrics_rewriter::StageMetricsRewriter;
