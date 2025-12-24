e893d518a3a71ef914b3fe8b62ceb43000818cc4
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2025-12-24 17:42:17 +0100

    Add ChildrenIsolatorUnionExec

--- src/execution_plans/mod.rs
+++ src/execution_plans/mod.rs
@@ -1,3 +1,4 @@
+mod children_isolator_union;
 mod common;
 mod distributed;
 mod metrics;
@@ -5,6 +6,7 @@ mod network_coalesce;
 mod network_shuffle;
 mod partition_isolator;
 
+pub use children_isolator_union::ChildrenIsolatorUnionExec;
 pub use distributed::DistributedExec;
 pub(crate) use metrics::MetricsWrapperExec;
 pub use network_coalesce::NetworkCoalesceExec;
