25b81ab9e93c5bc947b964e0e8d8953852121b5d
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2026-01-07 17:50:00 +0100

    Allow passing a custom query

--- benchmarks/src/run.rs
+++ benchmarks/src/run.rs
@@ -145,8 +145,8 @@ impl Dataset {
     fn queries(&self) -> Result<Vec<(&'static str, String)>, DataFusionError> {
         match self {
             Dataset::Tpch => [
-                "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13",
-                "q14", "q15", "q16", "q17", "q18", "q19", "q20", "q21", "q22",
+                "custom1", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11",
+                "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19", "q20", "q21", "q22",
             ]
             .into_iter()
             .map(|id| Ok((id, tpch::get_tpch_query(id)?)))
