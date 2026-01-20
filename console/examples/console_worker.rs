e063cff8f0b47c6ff26acfbfcd75e6940e2134f5
Edson Petry <edson.petry@datadoghq.com>
2026-01-20 12:30:12 -0500

    doc: added examples/ to console with documentation on how to run the crate

---
+++ console/examples/console_worker.rs
@@ -0,0 +1,33 @@
+use arrow_flight::flight_service_server::FlightServiceServer;
+use datafusion_distributed::{ObservabilityServiceServer, SharedWorker, Worker};
+use std::error::Error;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use structopt::StructOpt;
+use tonic::transport::Server;
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "console_worker",
+    about = "A localhost DataFusion worker with observability"
+)]
+struct Args {
+    #[structopt(default_value = "8080")]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::from_args();
+
+    let worker = SharedWorker::new(Worker::default());
+    let flight_service = FlightServiceServer::new(worker.clone());
+    let observability_service = ObservabilityServiceServer::new(worker.clone());
+
+    Server::builder()
+        .add_service(flight_service)
+        .add_service(observability_service)
+        .serve(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), args.port))
+        .await?;
+
+    Ok(())
+}
