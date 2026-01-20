7add867822ef65728786b71717e2cdfad6cb2ce9
Edson Petry <edson.petry@datadoghq.com>
2026-01-20 10:40:01 -0500

    fix: remove unused import

--- src/protobuf/mod.rs
+++ src/protobuf/mod.rs
@@ -6,10 +6,7 @@ mod user_codec;
 
 pub(crate) use app_metadata::{AppMetadata, FlightAppMetadata, MetricsCollection, TaskMetrics};
 pub(crate) use distributed_codec::{DistributedCodec, StageKey};
-pub(crate) use errors::{
-    datafusion_error_to_tonic_status, map_flight_to_datafusion_error,
-    map_status_to_datafusion_error,
-};
+pub(crate) use errors::{datafusion_error_to_tonic_status, map_flight_to_datafusion_error};
 pub use observability::observability_service_client::ObservabilityServiceClient;
 pub use observability::observability_service_server::ObservabilityServiceServer;
 pub use observability::{PingRequest, PingResponse};
