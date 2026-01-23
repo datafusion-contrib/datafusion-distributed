1e3074a4f3fd2d2f995ddaffb26713decfd15043
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2026-01-23 21:46:58 +0100

    Add compute_per_node.rs

--- src/distributed_planner/statistics/mod.rs
+++ src/distributed_planner/statistics/mod.rs
@@ -1,3 +1,6 @@
 mod bytes_per_row;
+mod compute_per_node;
 
 pub(crate) use bytes_per_row::bytes_per_row;
+pub use compute_per_node::ComputeCost;
+pub(crate) use compute_per_node::compute_cost_for_node;
