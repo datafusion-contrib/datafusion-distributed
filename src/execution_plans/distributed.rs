1a6c2d872bf12a74c1d1f17cab0b3957c8bb6adb
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2026-02-23 08:30:47 +0100

    Better metrics

--- src/execution_plans/distributed.rs
+++ src/execution_plans/distributed.rs
@@ -12,6 +12,7 @@ use crate::{
 };
 use arrow_flight::Action;
 use bytes::Bytes;
+use datafusion::common::instant::Instant;
 use datafusion::common::runtime::JoinSet;
 use datafusion::common::tree_node::{Transformed, TreeNode};
 use datafusion::common::{Result, exec_err, internal_err};
@@ -19,20 +20,23 @@ use datafusion::common::{exec_datafusion_err, internal_datafusion_err};
 use datafusion::error::DataFusionError;
 use datafusion::execution::{SendableRecordBatchStream, TaskContext};
 use datafusion::physical_expr_common::metrics::MetricsSet;
-use datafusion::physical_plan::metrics::{ExecutionPlanMetricsSet, Label, MetricBuilder};
+use datafusion::physical_plan::metrics::{
+    ExecutionPlanMetricsSet, Label, MetricBuilder, MetricValue, Time,
+};
 use datafusion::physical_plan::stream::RecordBatchReceiverStreamBuilder;
 use datafusion::physical_plan::{DisplayAs, DisplayFormatType, ExecutionPlan, PlanProperties};
 use datafusion_proto::physical_plan::AsExecutionPlan;
 use datafusion_proto::protobuf::PhysicalPlanNode;
-use futures::future::BoxFuture;
-use futures::{FutureExt, StreamExt};
+use futures::StreamExt;
 use http::Extensions;
 use prost::Message;
 use rand::Rng;
 use std::any::Any;
-use std::fmt::Formatter;
+use std::fmt::{Display, Formatter};
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::time::Duration;
 use tonic::Request;
 use tonic::metadata::MetadataMap;
 use url::Url;
@@ -52,7 +56,7 @@ pub struct DistributedExec {
 
 struct PreparedPlan {
     plan: Arc<dyn ExecutionPlan>,
-    tasks: Vec<BoxFuture<'static, Result<()>>>,
+    join_set: JoinSet<Result<()>>,
 }
 
 impl DistributedExec {
@@ -76,6 +80,13 @@ impl DistributedExec {
             })
     }
 
+    /// Prepares the distributed plan for execution, which implies:
+    /// 1. Perform some worker assignation, choosing randomly from the given URLs and assigning one
+    ///    URL per task.
+    /// 2. Sending the sliced subplans to the assigned URLs. For each URL assigned to a task, a
+    ///    network call feeding the subplan is necessary.
+    /// 3. In each network boundary, set the input plan to `None`. That way, network boundaries
+    ///    become nodes without children and traversing them will not go further down in.
     fn prepare_plan(&self, ctx: &Arc<TaskContext>) -> Result<PreparedPlan> {
         let worker_resolver = get_distributed_worker_resolver(ctx.session_config())?;
         let codec = DistributedCodec::new_combined_with_user(ctx.session_config());
@@ -84,23 +95,37 @@ impl DistributedExec {
         headers.extend(get_passthrough_headers(ctx.session_config()));
         let urls = worker_resolver.get_urls()?;
 
-        let mut tasks = vec![];
-
+        // Metric that measures to total sum of bytes worth of subplans sent.
         let plan_bytes_sent = MetricBuilder::new(&self.metrics)
             .with_label(Label::new(DISTRIBUTED_DATAFUSION_TASK_ID_LABEL, "0"))
             .global_counter("plan_bytes_sent");
 
+        // Latency statistics about the network calls issued to the workers for feeding subplans.
+        let start = Instant::now();
+        let plan_send_latency = Arc::new(LatencyMetric::new(
+            "plan_send_latency",
+            |b| b.with_label(Label::new(DISTRIBUTED_DATAFUSION_TASK_ID_LABEL, "0")),
+            &self.metrics,
+        ));
+
+        let mut join_set = JoinSet::new();
         let prepared = Arc::clone(&self.plan).transform_up(|plan| {
+            // The following logic is just applied on network boundaries.
             let Some(plan) = plan.as_network_boundary() else {
                 return Ok(Transformed::no(plan));
             };
+
             let stage = plan.input_stage();
             let Some(input_plan) = &stage.plan else {
                 return internal_err!("Plan is not set for stage {}", stage.num);
             };
 
+            // Right now, we assign random workers to tasks. This might change in the future.
             let start_idx = rand::rng().random_range(0..urls.len());
 
+            // This assumes the plan is the same for all the tasks within a stage. This is fine for
+            // now, but it should be possible to send different versions of the subplan to the
+            // different tasks.
             let bytes: Bytes =
                 PhysicalPlanNode::try_from_physical_plan(Arc::clone(input_plan), &codec)?
                     .encode_to_vec()
@@ -112,6 +137,9 @@ impl DistributedExec {
                 .enumerate()
                 .map(|(i, _)| {
                     let url = urls[(start_idx + i) % urls.len()].clone();
+                    let execution_task = ExecutionTask {
+                        url: Some(url.clone()),
+                    };
                     let action = InitAction {
                         plan_proto: bytes.clone(),
                         stage_key: Some(StageKey {
@@ -121,9 +149,16 @@ impl DistributedExec {
                         }),
                     };
                     plan_bytes_sent.add(bytes.len());
-                    tasks.push(send_plan_task(Arc::clone(ctx), url.clone(), action).boxed());
-
-                    ExecutionTask { url: Some(url) }
+                    let plan_send_latency = Arc::clone(&plan_send_latency);
+                    let ctx = Arc::clone(ctx);
+                    // Spawns the task that feeds this subplan to this worker. There will be as
+                    // many as this spawned tasks as workers.
+                    join_set.spawn(async move {
+                        send_plan_task(ctx, url, action).await?;
+                        plan_send_latency.record(&start);
+                        Ok(())
+                    });
+                    execution_task
                 })
                 .collect::<Vec<_>>();
 
@@ -136,7 +171,7 @@ impl DistributedExec {
         })?;
         Ok(PreparedPlan {
             plan: prepared.data,
-            tasks,
+            join_set,
         })
     }
 }
@@ -190,7 +225,7 @@ impl ExecutionPlan for DistributedExec {
             );
         }
 
-        let PreparedPlan { plan, tasks } = self.prepare_plan(&context)?;
+        let PreparedPlan { plan, join_set } = self.prepare_plan(&context)?;
         {
             let mut guard = self
                 .prepared_plan
@@ -200,6 +235,7 @@ impl ExecutionPlan for DistributedExec {
         }
         let mut builder = RecordBatchReceiverStreamBuilder::new(self.schema(), 1);
         let tx = builder.tx();
+        // Spawn the task that pulls data from child...
         builder.spawn(async move {
             let mut stream = plan.execute(partition, context)?;
             while let Some(msg) = stream.next().await {
@@ -209,11 +245,8 @@ impl ExecutionPlan for DistributedExec {
             }
             Ok(())
         });
+        // ...in parallel to the one that feeds the plan to workers.
         builder.spawn(async move {
-            let mut join_set = JoinSet::new();
-            for task in tasks {
-                join_set.spawn(task);
-            }
             for res in join_set.join_all().await {
                 res?;
             }
@@ -249,4 +282,59 @@ async fn send_plan_task(ctx: Arc<TaskContext>, url: Url, init_action: InitAction
             .unwrap_or_else(|| exec_datafusion_err!("Error sending plan to worker {url}: {e}"))
     })?;
     Ok(())
+}
+
+/// DataFusion metrics system is pretty limited from an API standpoint. This intermediate struct
+/// bridges the gaps that are not satisfied by upstream API for measuring latency.
+struct LatencyMetric {
+    max: Time,
+    avg: Time,
+    max_latency_micros: AtomicU64,
+    sum_latency_micros: AtomicU64,
+    count_latency_micros: AtomicU64,
+}
+
+impl Drop for LatencyMetric {
+    fn drop(&mut self) {
+        self.max.add_duration(Duration::from_micros(
+            self.max_latency_micros.load(Ordering::Relaxed),
+        ));
+        self.avg.add_duration(Duration::from_micros(
+            self.sum_latency_micros.load(Ordering::Relaxed)
+                / self.count_latency_micros.load(Ordering::Relaxed).max(1),
+        ));
+    }
+}
+
+impl LatencyMetric {
+    fn new(
+        name: impl Display,
+        builder: impl Fn(MetricBuilder) -> MetricBuilder,
+        metrics: &ExecutionPlanMetricsSet,
+    ) -> Self {
+        let max = Time::new();
+        builder(MetricBuilder::new(metrics)).build(MetricValue::Time {
+            name: format!("{name}_max").into(),
+            time: max.clone(),
+        });
+        let avg = Time::new();
+        builder(MetricBuilder::new(metrics)).build(MetricValue::Time {
+            name: format!("{name}_avg").into(),
+            time: avg.clone(),
+        });
+        Self {
+            max,
+            avg,
+            max_latency_micros: AtomicU64::new(0),
+            sum_latency_micros: AtomicU64::new(0),
+            count_latency_micros: AtomicU64::new(0),
+        }
+    }
+
+    fn record(&self, start: &Instant) {
+        let micros = start.elapsed().as_micros() as u64;
+        self.max_latency_micros.fetch_max(micros, Ordering::Relaxed);
+        self.sum_latency_micros.fetch_add(micros, Ordering::Relaxed);
+        self.count_latency_micros.fetch_add(1, Ordering::Relaxed);
+    }
 }
