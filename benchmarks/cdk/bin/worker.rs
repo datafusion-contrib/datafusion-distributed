9b4dd5ae2eef21af5d4500fd3cc9c031624d9077
Edson Petry <edson.petry@datadoghq.com>
2026-03-17 11:31:51 -0400

    feat: add observability service to benchmark ec2 worker

--- benchmarks/cdk/bin/worker.rs
+++ benchmarks/cdk/bin/worker.rs
@@ -206,8 +206,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 }),
             ),
     );
+    let ec2_worker_resolver = Arc::new(Ec2WorkerResolver::new());
     let grpc_server = Server::builder()
-        .add_service(worker.with_observability_service(Ec2WorkerResolver::new()))
+        .add_service(worker.with_observability_service(ec2_worker_resolver))
         .add_service(worker.into_flight_server())
         .serve(worker_addr.parse()?);
 
