46d095408375389d2241a1e1baecfc31f190adb3
Edson Petry <edson.petry@datadoghq.com>
2026-03-19 18:07:58 -0400

    doc: add example for worker_has_version helper function

--- src/networking/version.rs
+++ src/networking/version.rs
@@ -7,6 +7,24 @@ use super::ChannelResolver;
 /// Checks whether a worker at the given URL reports the expected version by calling
 /// the [`WorkerService::GetWorkerInfo`] RPC.
 ///
+/// ```ignore
+/// // In a background task, periodically filter worker URLs by version:
+/// let channel_resolver = DefaultChannelResolver::default();
+/// let compatible_urls: Arc<RwLock<Vec<Url>>> = /* shared with WorkerResolver */;
+///
+/// tokio::spawn(async move {
+///     loop {
+///         let mut filtered = vec![];
+///         for url in &all_known_urls {
+///             if worker_has_version(&channel_resolver, url, "1.0").await {
+///                 filtered.push(url.clone());
+///             }
+///         }
+///         *compatible_urls.write().unwrap() = filtered;
+///         tokio::time::sleep(Duration::from_secs(5)).await;
+///     }
+/// });
+/// ```
 /// Returns `false` if the worker is unreachable, returns an error, or reports a
 /// different version. This is intended to be used inside [`WorkerResolver`] implementations
 /// for filtering out workers with incompatible versions during rolling deployments.
