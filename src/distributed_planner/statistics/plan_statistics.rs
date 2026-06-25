1257d58758577b6a0789c9bf8c0bc9cf5c7f1e91
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2026-06-25 10:04:16 +0200

    Remove default workarounds

--- src/distributed_planner/statistics/plan_statistics.rs
+++ src/distributed_planner/statistics/plan_statistics.rs
@@ -1,6 +1,3 @@
-use crate::DistributedConfig;
-use crate::distributed_planner::statistics::default_bytes_for_datatype::default_bytes_for_datatype;
-use datafusion::common::stats::Precision;
 use datafusion::common::{Statistics, not_impl_err, plan_err};
 use datafusion::config::ConfigOptions;
 use datafusion::error::Result;
@@ -8,79 +5,8 @@ use datafusion::execution::{SendableRecordBatchStream, TaskContext};
 use datafusion::physical_plan::execution_plan::CardinalityEffect;
 use datafusion::physical_plan::{DisplayAs, DisplayFormatType, ExecutionPlan, PlanProperties};
 use delegate::delegate;
-use itertools::Itertools;
 use std::fmt::Formatter;
 use std::sync::Arc;
-
-/// Uses upstream DataFusion stats system with some small overrides.
-pub(super) fn plan_statistics(
-    node: &Arc<dyn ExecutionPlan>,
-    children_stats: &[Arc<Statistics>],
-    opts: &DistributedConfig,
-) -> Result<Arc<Statistics>> {
-    let mut stats = partition_statistics_with_children_override(node, None, children_stats)?;
-
-    // If rows are absent, be conservative and assume that all the rows from all the children
-    // are going to be returned.
-    if matches!(stats.num_rows, Precision::Absent) {
-        let num_rows = children_stats
-            .iter()
-            .flat_map(|v| v.num_rows.get_value())
-            .sum1::<usize>();
-        let num_rows = if let Some(num_rows) = num_rows {
-            num_rows
-        } else if let Some(default) = opts.default_estimated_row_count {
-            default
-        } else {
-            return plan_err!(
-                "{} does not provide row stats, and none of its children [{}] provides a row count",
-                node.name(),
-                node.children()
-                    .iter()
-                    .map(|v| v.name())
-                    .collect::<Vec<_>>()
-                    .join(", ")
-            );
-        };
-        stats.num_rows = Precision::Inexact(num_rows)
-    }
-
-    let schema = node.schema();
-
-    for (i, col_stats) in &mut stats.column_statistics.iter_mut().enumerate() {
-        let rows = stats.num_rows.get_value().unwrap_or(&0);
-
-        // If some of the NDVs are not present in one of the column-level stats, assume the
-        // worst and use the same as the input number of rows.
-        if matches!(col_stats.distinct_count, Precision::Absent) {
-            col_stats.distinct_count = Precision::Inexact(*rows);
-        }
-
-        // If the per-column byte size stats are not present, estimate the byte size based on the
-        // data type and the row count.
-        let Some(dt) = schema.fields.get(i).map(|v| v.data_type()) else {
-            return plan_err!("Field with index {i} not present in schema: {schema:?}");
-        };
-
-        // If it turns out that we do not have `byte_size` stats, but we do have an estimated number
-        // of rows, do a best-effort in trying to infer the byte size for each column.
-        if matches!(col_stats.byte_size, Precision::Absent) {
-            col_stats.byte_size = Precision::Inexact(default_bytes_for_datatype(dt) * rows)
-        }
-    }
-
-    // If bytes are absent, let's just infer them based on the schema and the
-    // number of rows.
-    if matches!(stats.total_byte_size, Precision::Absent) {
-        let mut total_byte_size = 0;
-        for col_stats in &stats.column_statistics {
-            total_byte_size += col_stats.byte_size.get_value().unwrap_or(&0);
-        }
-        stats.total_byte_size = Precision::Inexact(total_byte_size);
-    }
-
-    Ok(Arc::new(stats))
-}
 
 // FIXME: because of limitations the the statistics API on DataFusion, we need to resource to
 //  this sketchy way of overriding child statistics, as we cannot just provide our own.
@@ -89,7 +15,7 @@ pub(super) fn plan_statistics(
 //   2. we recompute statistics unnecessarily across the plan
 //  This is tracked by https://github.com/apache/datafusion/issues/20184 upstream, and until
 //  that one is solved, we need to resource to this wrapper.
-fn partition_statistics_with_children_override(
+pub(crate) fn partition_statistics_with_children_override(
     node: &Arc<dyn ExecutionPlan>,
     partition: Option<usize>,
     child_stats: &[Arc<Statistics>],
