b764c995b00a99d0bdd3fbb7a9367c7a7326b1a8
Gabriel <gabriel.musatmestre@datadoghq.com>
2026-06-30 12:24:40 +0200

    Use Precision for Cost

--- src/distributed_planner/statistics/cost.rs
+++ src/distributed_planner/statistics/cost.rs
@@ -3,22 +3,23 @@ use crate::distributed_planner::statistics::complexity_memory::complexity_memory
 use crate::distributed_planner::statistics::complexity_network::complexity_network;
 use crate::distributed_planner::statistics::plan_statistics::plan_statistics;
 use datafusion::common::Result;
+use datafusion::common::stats::Precision;
 use datafusion::physical_plan::{ExecutionPlan, Statistics};
 use std::ops::AddAssign;
 use std::sync::Arc;
 
 #[derive(Default)]
 pub(crate) struct Cost {
-    pub(crate) cpu: usize,
-    pub(crate) memory: usize,
-    pub(crate) network: usize,
+    pub(crate) cpu: Precision<usize>,
+    pub(crate) memory: Precision<usize>,
+    pub(crate) network: Precision<usize>,
 }
 
 impl AddAssign for Cost {
     fn add_assign(&mut self, rhs: Self) {
-        self.cpu = self.cpu.saturating_add(rhs.cpu);
-        self.memory = self.memory.saturating_add(rhs.memory);
-        self.network = self.network.saturating_add(rhs.network);
+        self.cpu = self.cpu.add(&rhs.cpu);
+        self.memory = self.memory.add(&rhs.memory);
+        self.network = self.network.add(&rhs.network);
     }
 }
 
@@ -37,17 +38,18 @@ fn f(plan: &Arc<dyn ExecutionPlan>) -> Result<(Cost, Arc<Statistics>)> {
     }
 
     let stats = plan_statistics(plan, &child_stats)?;
-    let cpu = complexity_cpu(plan);
-    acc_cost.cpu = acc_cost
-        .cpu
-        .saturating_add(cpu.cost(&stats, &child_stats).unwrap_or(0));
-    let memory = complexity_memory(plan);
-    acc_cost.memory = acc_cost
-        .memory
-        .saturating_add(memory.cost(&stats, &child_stats).unwrap_or(0));
-    let network = complexity_network(plan);
-    acc_cost.network = acc_cost
-        .network
-        .saturating_add(network.cost(&stats, &child_stats).unwrap_or(0));
+    let new_cost = Cost {
+        cpu: inexact_or_absent(complexity_cpu(plan).cost(&stats, &child_stats)),
+        memory: inexact_or_absent(complexity_memory(plan).cost(&stats, &child_stats)),
+        network: inexact_or_absent(complexity_network(plan).cost(&stats, &child_stats)),
+    };
+    acc_cost += new_cost;
     Ok((acc_cost, stats))
+}
+
+fn inexact_or_absent(value: Option<usize>) -> Precision<usize> {
+    match value {
+        None => Precision::Absent,
+        Some(v) => Precision::Inexact(v),
+    }
 }
