40c52aefb7035ea51cc984bac0f252a8c9c0337f
Jayan Shrivastava <jshrivastava03@gmail.com>
2026-08-23 15:16:22 -0400

    refactor: rename dynamic filter partial update method

--- src/coordinator/query_coordinator.rs
+++ src/coordinator/query_coordinator.rs
@@ -299,7 +299,7 @@ impl<'a> StageCoordinator<'a> {
                         }
                     }
                     WorkerToCoordinatorMsg::ProducedDynamicFilter(filter) => {
-                        dynamic_filter_registry.update(task_key, *filter);
+                        dynamic_filter_registry.add_partial(task_key, *filter);
                     }
                 }
             }
