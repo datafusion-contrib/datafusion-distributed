5683abd626a9d24cc33c582b52e44dfef25b46ed
Gabriel <gabriel.musatmestre@datadoghq.com>
2026-09-10 09:53:56 +0200

    Implement Copy on WorkUnitFeedDeclaration

--- src/protocol/worker_channel.rs
+++ src/protocol/worker_channel.rs
@@ -65,7 +65,7 @@ pub struct TaskKey {
     pub task_number: usize,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct WorkUnitFeedDeclaration {
     /// Unique identifier of the node to which work unit feeds are expected to be streamed.
     pub id: Uuid,
