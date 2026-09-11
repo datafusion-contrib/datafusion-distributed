982593ff3e78d355b303c5ecdc573f64977b4483
Jayant Shrivastava <jayant.shrivastava@datadoghq.com>
2026-09-11 18:01:03 +0000

    better comments around dynamic filter "sever" functions

--- src/dynamic_filtering/display.rs
+++ src/dynamic_filtering/display.rs
@@ -51,6 +51,9 @@ pub async fn rewrite_distributed_plan_with_dynamic_filters(
 /// Severs dynamic filter connections so we can update filter values for
 /// display purposes without having an update in one node propagate to another.
 ///
+/// Unlike [`maybe_roundtrip_plan_to_sever_in_memory_dynamic_filter_relationships()`], this
+/// isolates every dynamic filter found in the plan.
+///
 /// For example, in this plan, we would like to be able to [`update()`] every variant independently
 /// without mutating the producer or other variants
 ///
@@ -62,14 +65,15 @@ pub async fn rewrite_distributed_plan_with_dynamic_filters(
 ///          t0: DataSourceExec: ...
 ///          t1: DataSourceExec: ...
 ///        DistributedLeafExec:
-///          t0: DataSourceExec:  predicate=DynamicFilter [ f_dkey@2 >= A AND f_dkey@2 <= A AND f_dkey@2 IN (SET) ([<values>]) ] <- unique filter
-///          t1: DataSourceExec:  predicate=DynamicFilter [ f_dkey@2 >= B AND f_dkey@2 <= B AND f_dkey@2 IN (SET) ([<values>]) ] <- unique filter
+///          t0: DataSourceExec:  predicate=DynamicFilter [ f_dkey@2 > A ] <- unique filter
+///          t1: DataSourceExec:  predicate=DynamicFilter [ f_dkey@2 < B ] <- unique filter
 /// ```
 ///
 /// This is done by deep-copying every leaf variant so we don't have to
 /// worry about any shared state.
 ///
 /// [`update()`]: DynamicFilterPhysicalExpr::update()
+/// [`maybe_roundtrip_plan_to_sever_in_memory_dynamic_filter_relationships()`]: super::maybe_roundtrip_plan_to_sever_in_memory_dynamic_filter_relationships()
 pub(crate) fn sever_dynamic_filter_relationships_in_plan_for_display(
     plan: Arc<dyn ExecutionPlan>,
     task_ctx: &Arc<TaskContext>,
--- src/dynamic_filtering/mod.rs
+++ src/dynamic_filtering/mod.rs
@@ -11,72 +11,76 @@ pub(crate) use discovery::*;
 pub use display::rewrite_distributed_plan_with_dynamic_filters;
 pub(crate) use display::sever_dynamic_filter_relationships_in_plan_for_display;
 
-// We must take care to avoid partial dynamic filter updates when sending an
-// in-memory plan. Because there's many failure modes, the safest option is
-// to roundtrip the plan when any dynamic filter is found.
-//
-// The purpose of this function is to sever any possible cross-task in-memory
-// dynamic filter relationships.
-//
-// Example 1: Producer-Consumer
-//
-// Consider this partitioned hash join topology where the consumer task is
-// collocated with one producer on worker A:
-// ```text
-// Worker A
-//
-// Stage 2 Task 0
-// HashJoinExec <- Dynamic Filter Produced: (foo > 100)
-//
-// Stage 1 Task 0
-// DataSourceExec <- consumer
-//
-// Worker B
-// Stage 2 Task 1
-// HashJoinExec <- Dynamic Filter Produced: (foo != 150)
-// ```
-//
-// The in-process transport allows the Worker A join to propagate its filter to
-// the consumer and mark it as completed, so the consumer incorrectly applies
-// (foo > 100) instead of (foo > 100 OR foo != 150).
-//
-// Example 2: Producer-Producer
-//
-// ```text
-// Worker A
-//
-// Stage 2 Task 0
-// HashJoinExec <- Dynamic Filter Produced: (foo > 100)
-//
-// Stage 2 Task 1
-// HashJoinExec <- Dynamic Filter Produced: (foo != 150)
-//
-// Stage 1 Task 0
-// DataSourceExec <- consumer
-// ```
-//
-// Both producers are collocated on worker A. In this situation, they both race to
-// update the dynamic filter, meaning the final expression will either be foo > 100
-// or foo != 150. The correct expression is (foo > 100 OR foo != 150).
-//
-// Example 3: Local Producer-Consumer
-//
-// ```text
-// Worker A
-//
-// Stage 2 Task 0
-// HashJoinExec <- Dynamic Filter Produced: (foo > 100)
-//   DataSourceExec <- consumer
-//
-// Stage 2 Task 1
-// HashJoinExec <- Dynamic Filter Produced: (foo != 150)
-//   DataSourceExec <- consumer
-// ```
-//
-// Since both producers and both consumers are located on the same worker, they all share
-// one in-memory dynamic filter. This ends up being a race between two writers and two readers.
-// For a partitioned hash join, a producer may update its task-local consumer in memory, but
-// updates must not cross task boundaries.
+/// Removes all shared, in-memory dynamic filter state from this plan when it contains
+/// any dynamic filter producers or consumers. Plan nodes *within* this plan will share
+/// in-memory dynamic filter state. However, they will not share state with plan nodes
+/// outside of this plan, such as in parent stages.
+///
+/// Since it's possible for a user to implement a custom transport layer generally
+/// skipping proto serialization, we assume that any two tasks can be collocated
+/// and share memory. Aside from this, the only reason any two tasks can be collocated
+/// and share memory is if they are both on the coordinator and the coordinator sends
+/// the plan via an in-memory channel.
+///
+/// ## Example 1: Producer-Consumer
+///
+/// Consider this partitioned hash join topology where the consumer task is
+/// collocated with one producer on worker A:
+/// ```text
+/// Worker A
+///
+/// Stage 2 Task 0
+/// HashJoinExec <- Dynamic Filter Produced: (foo > 100)
+///
+/// Stage 1 Task 0
+/// DataSourceExec <- consumer
+///
+/// Worker B
+/// Stage 2 Task 1
+/// HashJoinExec <- Dynamic Filter Produced: (foo != 150)
+/// ```
+///
+/// The in-process transport allows the Worker A join to propagate its filter to
+/// the consumer and mark it as completed, so the consumer incorrectly applies
+/// (foo > 100) instead of (foo > 100 OR foo != 150).
+///
+/// ## Example 2: Producer-Producer
+///
+/// ```text
+/// Worker A
+///
+/// Stage 2 Task 0
+/// HashJoinExec <- Dynamic Filter Produced: (foo > 100)
+///
+/// Stage 2 Task 1
+/// HashJoinExec <- Dynamic Filter Produced: (foo != 150)
+///
+/// Stage 1 Task 0
+/// DataSourceExec <- consumer
+/// ```
+///
+/// Both producers are collocated on worker A. In this situation, they both race to
+/// update the dynamic filter, meaning the final expression will either be foo > 100
+/// or foo != 150. The correct expression is (foo > 100 OR foo != 150).
+///
+/// Example 3: Local Producer-Consumer
+///
+/// ```text
+/// Worker A
+///
+/// Stage 2 Task 0
+/// HashJoinExec <- Dynamic Filter Produced: (foo > 100)
+///   DataSourceExec <- consumer
+///
+/// Stage 2 Task 1
+/// HashJoinExec <- Dynamic Filter Produced: (foo != 150)
+///   DataSourceExec <- consumer
+/// ```
+///
+/// Since both producers and both consumers are located on the same worker, they all share
+/// one in-memory dynamic filter. This ends up being a race between two writers and two readers.
+/// For a partitioned hash join, a producer may update its task-local consumer in memory, but
+/// updates must not cross task boundaries.
 pub(crate) fn maybe_roundtrip_plan_to_sever_in_memory_dynamic_filter_relationships(
     plan: Arc<dyn ExecutionPlan>,
     task_ctx: &Arc<TaskContext>,
