bfd9b709ca3305297b7127e3a2f6b2fe442b267c
Stu Hood <stuhood@gmail.com>
2026-09-16 14:02:35 -0700

    Add range benchmarks.

--- src/lib.rs
+++ src/lib.rs
@@ -75,5 +75,5 @@ pub use worker::{
 #[cfg(any(feature = "integration", test))]
 pub use execution_plans::benchmarks::{
     LocalRepartitionBench, LocalRepartitionFixture, LocalRepartitionMode, ShuffleBench,
-    ShuffleFixture, TransportBench, TransportBenchMode, TransportFixture,
+    ShuffleFixture, ShufflePartitioningMode, TransportBench, TransportBenchMode, TransportFixture,
 };
