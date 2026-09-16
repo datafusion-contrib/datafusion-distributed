4ad3c6442c482ac7e031455a09de115972f3f57d
RIchard Baah <richard.baah@datadoghq.com>
2026-09-16 11:07:12 -0400

    fmt

--- src/codec/distributed_codec.rs
+++ src/codec/distributed_codec.rs
@@ -605,8 +605,12 @@ fn new_network_hash_shuffle_exec(
 ) -> NetworkShuffleExec {
     let producer_tasks = input_stage.task_count();
     let consumer_partitions = partitioning.partition_count();
-    let salted =
-        should_use_salted_mode(producer_tasks, consumer_task_count, consumer_partitions, max_mn_for_direct);
+    let salted = should_use_salted_mode(
+        producer_tasks,
+        consumer_task_count,
+        consumer_partitions,
+        max_mn_for_direct,
+    );
     let (mode, output_partitions) = if !salted {
         (ShuffleMode::Direct, consumer_partitions)
     } else {
